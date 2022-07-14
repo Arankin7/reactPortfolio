6d90050377f96d09f492fb2bb8a1da9a03eb1998
AnthonyR <arankin789@gmail.com>
2022-07-14 12:19:20 -0400

    initial component setup

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    
+    return (
+        <section>
+            <h1>Who is Anthony Rankin?</h1>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav(){
+
+    return (
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Nav;
